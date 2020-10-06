9a7d6949a583ef9ccc72b780078a6c95d809fd06
Dave Uhlik <49681728+daveuhlik@users.noreply.github.com>
2020-10-06 09:37:25 +0200

    Add files via upload

--- lottery.js
+++ lottery.js
@@ -1,11 +1,15 @@
 const button = document.getElementById('button');
 const counter = document.getElementById('counter');
 const actualNumber = document.getElementById('number');
+let numberList = [];
 
 let randomNumber = Math.floor(Math.random() * 99);
 
 button.addEventListener('click', function() {
-	counter.textContent = randomNumber;
 	actualNumber.textContent = randomNumber;
 	actualNumber.classList.add('big-num');
+	counter.textContent = randomNumber;
+	counter.unshift(randomNumber)
+	console.log(counter.value);
 })
+
